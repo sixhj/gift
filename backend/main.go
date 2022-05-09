41e85be99a3c6dc1acfac78ee5a9cdce23602e60
a <sixhj@gmail.com>
2022-05-09 23:44:09 +0800

    update

--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,10 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	engine := gin.New()
+	engine := gin.Default()
 	engine.POST("/hello", func(context *gin.Context) {
 		context.JSON(200, "hello")
 		return
 	})
+	engine.Run()
 }
